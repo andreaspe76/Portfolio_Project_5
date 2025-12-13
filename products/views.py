a00042e4ab443d8e6fa42f1a4b1f10b7618293f6
Andreas Pergantis <andreaspergantis@hotmail.com>
2025-12-13 11:25:42 +0100

    Connect with database

--- products/views.py
+++ products/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def product_list(request):
+    return HttpResponse("Hello, this will be the product list page.")
