12a334ef50bc0a4ee715a0858e86255013d334b9
Andreas Pergantis <andreaspergantis@hotmail.com>
2025-12-20 19:13:09 +0100

    Fix static image handling for Heroku

--- hifi_store/settings.py
+++ hifi_store/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
 
--- products/models.py
+++ products/models.py
@@ -19,7 +19,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     description = models.TextField()
-    image = models.ImageField(upload_to='products/')
+    image = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
