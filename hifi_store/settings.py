859aa56c237c182cc740274c20a0596872f54743
Andreas Pergantis <andreaspergantis@hotmail.com>
2025-12-22 18:10:48 +0100

    Prepare for stripe integration

--- hifi_store/settings.py
+++ hifi_store/settings.py
@@ -92,6 +92,10 @@ DATABASES = {
     )
 }
 
+# Stripe settings
+STRIPE_PUBLIC_KEY = os.environ.get("STRIPE_PUBLIC_KEY")
+STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
