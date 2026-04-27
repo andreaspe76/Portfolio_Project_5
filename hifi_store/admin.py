ad3fced7036e755624a101d2b38c5f88c5d5e3df
Andreas Pergantis <andreaspergantis@hotmail.com>
2026-04-27 18:52:05 +0200

    Register Site model in admin for production sitemap configuration

--- hifi_store/admin.py
+++ hifi_store/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import NewsletterSubscription
+from django.contrib.sites.models import Site
+
+admin.site.register(Site)
 
 
 @admin.register(NewsletterSubscription)
