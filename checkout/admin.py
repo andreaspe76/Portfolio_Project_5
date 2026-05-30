354f19cdcca107ccd0e8409050b67491769c2f98
Andreas Pergantis <andreaspergantis@hotmail.com>
2026-05-30 11:41:57 +0200

    Register order and order item models in admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
 # Register your models here.
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "full_name", "email", "total", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("full_name", "email", "id")
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ("order", "product", "quantity", "price")
+    list_filter = ("order", "product")
