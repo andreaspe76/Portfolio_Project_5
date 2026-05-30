fb020a2f55585129b9504ccccef8048ca3e3d1ed
Andreas Pergantis <andreaspergantis@hotmail.com>
2026-05-30 12:54:30 +0200

    Create edit_profile view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .models import UserProfile
+from .forms import ProfileForm
 
-# Create your views here.
+
+@login_required
+def edit_profile(request):
+    profile = UserProfile.objects.get(user=request.user)
+
+    if request.method == "POST":
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect("edit_profile")
+    else:
+        form = ProfileForm(instance=profile)
+
+    return render(request, "accounts/edit_profile.html", {"form": form})
